daf80c261bd6040c8bc1effea56285675e3793c6
Ryan Braun <ryan@thelyricla.com>
2020-09-23 10:49:03 -0700

    forgot to save

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,16 +2,17 @@ const db = require("../models");
 
 module.exports = function (app) {
 
-
+    //router put?
     app.put("/api/workouts/:id", (req, res) => {
         
         db.Workout.findOneAndUpdate(
             {_id: req.params.id},
-            { 
-                $inc: {totalDuration: req.body.duration},
+            { $inc: {totalDuration: req.body.duration},
+            // upsert:true, useFindeandModify:false
                 $push: {exercises: req.body}
             },
             { new: true }).then(dbWorkout => {
+                //console.log(dbWorkout)
                 res.json(dbWorkout);
             }).catch(err => {
                 res.json(err);
@@ -22,15 +23,18 @@ module.exports = function (app) {
     app.get ("/api/workouts", (req, res) => {
         
         db.Workout.find({}).then(dbWorkout => {
+            //db.Workout.find({}??
             dbWorkout.forEach(workout => {
                 var total = 0;
                 workout.exercises.forEach(e => {
                     total += e.duraction;
                 });
                 workout.totalDuration = total;
+                //console.log(total)
             });
 
             res.json(dbWorkout);
+            //console.log(dbWorkout)
         }).catch(err => {
             res.json(err);
         });
@@ -46,6 +50,9 @@ module.exports = function (app) {
     });
 
     app.post("/api/workouts", ({ body}, res) => {
+        //db.Workout.create({}).then(newWorkout => {
+            // res.json(newWorkout);
+       // })
         db.Workout.create(body).then((dbWorkout => {
             res.json(dbWorkout);
         })).catch(err => {
